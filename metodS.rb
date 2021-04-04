f7cc0f26db0a7998586dd6614bbf25ca90407106
ktpr <dmitrivanich@gmail.com>
2021-04-05 02:17:05 +0300

    skolko?

--- metodS.rb
+++ metodS.rb
@@ -1,7 +1,34 @@
 def sklonenie(number, il, ila, ilov)
-  
+  if (number == nil || !number.is_a?(Numeric))
+    number = 1
+  end
+
   ostatok = number % 10
-  i
+
+  if (number >= 5 && number <= 20)
+    return ilov
+  end
+  
+  if (ostatok == 1)
+    return il
+  end
+
+  if (ostatok >= 2 && ostatok <= 4)
+    return ila
+  end
+
+  if (ostatok >= 5 || ostatok == 0)
+    return ilov
+  end
 
 end
 
+skolko = ARGV[0].to_i
+
+skolko.times do 
+  puts "#{skolko} " + sklonenie(skolko, "удав", "удава", "удавов") + 
+  " " +
+  sklonenie(skolko, "пополз", "поползли", "поползли") +
+  " поплавать в море!"
+  skolko -= 1
+end
