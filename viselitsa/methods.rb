ee1c9b09d21196f79866ebf4662eb1b7087dda7e
ktpr <dmitrivanich@gmail.com>
2021-04-05 17:51:45 +0300

    0

--- viselitsa/methods.rb
+++ viselitsa/methods.rb
@@ -1,1 +1,18 @@
-puts "Аааа"
+def get_letters 
+
+  slovo = ARGV[0]
+
+  if (Gem.win_platform? and ARGV[0])
+    slovo = slovo.encode(ARGV[0].encoding, "cp1251").encode("UTF-8") 
+  end
+
+  if (slovo == nil || slovo == "")
+      abort ""
+  end
+
+  return slovo.split("")
+
+end
+
+
+puts get_letters.to_s
